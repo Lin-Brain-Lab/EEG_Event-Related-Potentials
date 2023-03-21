54e2dcee9ba92200f154a7b20cf8d951556fd81f
KJWoudsma <114693972+KJWoudsma@users.noreply.github.com>
2023-03-20 22:05:38 -0400

    Update BVDataPlot.m

--- scripts/BVDataPlot.m
+++ scripts/BVDataPlot.m
@@ -1,18 +1,31 @@
-function BVDataPlot (DATACALC, chanID, filedir)
+%% Script to create and save the MMNPlots for all datasets in a structure
+%Note: BVDataMMNPlot call may need to be edited based depending on dataset
+%naming conventions
+
+function BVDataPlot (DATACALC, ChanName, filedir)
+
+% inputs 
+%   DATACALC - a structure created with BVDataCalc with all desired datasets
+%   ChanName - a string indicating the desired channel to be plotted
+%   filedir - a string indicating the folder in which the figures should be saved
+% outputs
+%   figures for all datasets in DATACALC
+%   saved .png files for all figures
 
 
 
+close all % cleases any open figures
 
-close all
-
-% BVDataMMNPlot (DATACALC, dataID, ChanName, MMN, dERP, sERP)
+% BVDataMMNPlot (DATACALC, dataID, ChanName, MMN, eERP, nERP)
 for dataID = 1: length (DATACALC) % for all datasets in the structure DATA
     fig = figure('units','normalized','outerposition',[0 0 1 1]); %maximizes figure & window
-    subplot (2, 1, 1);
-    BVDataMMNPlot (DATACALC, dataID, chanID, 1, 3, 2);
-    subplot (2, 1, 2);
-    BVDataMMNPlot (DATACALC, dataID, chanID, 5, 4, 6);
-    file = (strcat (filedir, '\', chanID, '_', DATACALC(dataID).partinumb, '_mmn', '.png'));
-    saveas (fig, file);
-end
+    % plot figures
+    subplot (2, 1, 1); % places the following plot on the top position of the figure
+    BVDataMMNPlot (DATACALC, dataID, ChanName, 1, 3, 2); % plots the Angry MMN Plot
+    subplot (2, 1, 2); % places the following plot on the top position of the figure
+    BVDataMMNPlot (DATACALC, dataID, ChanName, 5, 4, 6); % plots the Sad MMN Plot
+    % save to folder
+    file = (strcat (filedir, '\', ChanName, '_', DATACALC(dataID).partinumb, '_mmn', '.png')); % defines the filename of the resultant figure
+    saveas (fig, file); % saves the figure
+end % ends the interative loop
 
